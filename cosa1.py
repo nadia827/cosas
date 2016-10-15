fbd97edd0b3e6ca2ba5bb5123045467c98269638
nadia827 <nadia.matias.827@gmail.com>
2016-10-15 19:41:39 -0300

    minpregunta de nombre
    solo pregunta el nombre

--- cosa1.py
+++ cosa1.py
@@ -1,3 +1,13 @@
 print("bien!")
 
+nombre = input("dime tu nombre")
+
+if nombre.lower() == "nadia":
+  print("te quiero.")
+else:
+  print("qué hacés tocando mi computadora!!")
+
+print("fin")
+
+
 
