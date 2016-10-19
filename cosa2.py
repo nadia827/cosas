6da3078cd5492e8d7c76adb1663d65d4660db18c
nadia827 <nadia.matias@gmail.com>
2016-10-19 13:46:54 -0300

    cosa2, loop

---
+++ cosa2.py
@@ -0,0 +1,7 @@
+a = 0 #a es igual a cero
+for i in range(5): # i tomará valores desde el cero al 4.
+    a = a + 1 #si a es cero y cero es el valor de i, a es igual a uno. en el segundo bucle, aaaah, los bucles se tienen que ejecutar (todos, los 5) y el valor que recibe a (al final del bucle) es el que se imprimirá.
+print(a)# ya entendí.
+#se comienza con a igual a cero, luego cuando se ejecuta por primera vez el bucle, a será igual a cero más uno, implica que a es igual a uno. Se ejecuta por segunda vez y como a tiene el valor de uno, le sumo uno más y ahora a es igual a 2. realizo esto otras 3 veces y a terminará teniendo el valor de 5. Fin.
+
+
